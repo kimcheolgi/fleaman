cc40219de639157f7bcecf37035d28c0506a9f6a
kimcheolgi <kimchelgi03@gmail.com>
2022-11-28 14:11:11 +0900

    로그인페이지 이미지 수정

--- src/page/Login.js
+++ src/page/Login.js
@@ -71,7 +71,7 @@ function Login() {
         <h4 className='mt-5'>
         <img
               alt=""
-              src="/logo.png"
+              src="/spin4.gif"
               width="30"
               height="30"
               className="d-inline-block align-top"

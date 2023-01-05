d8524daeab5ec491b4d226d07523ad2376c5127e
kimcheolgi <kimchelgi03@gmail.com>
2023-01-05 11:35:48 +0900

    스크립트 위치 수정

--- src/App.js
+++ src/App.js
@@ -100,9 +100,11 @@ function App() {
 
   useEffect(()=>{
     let inner = document.createElement('script');
+    let scr = document.createElement("script")
+    scr.src = "https://ads-partners.coupang.com/g.js"
     inner.innerHTML = `new PartnersCoupang.G({"id":629094,"template":"carousel","trackingCode":"AF7144675","width":"100%","height":"100%"})`;
     document.querySelector('.coupang').appendChild(inner);
-
+    document.querySelector('.coupang').appendChild(scr);
   }, [])
 
   return (

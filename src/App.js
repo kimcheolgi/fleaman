7fa119cec6fb665f7f761e76a6d295d28d609853
kimcheolgi <kimchelgi03@gmail.com>
2023-01-05 11:19:18 +0900

    광고 수정

--- src/App.js
+++ src/App.js
@@ -99,11 +99,8 @@ function App() {
   );
 
   useEffect(()=>{
-    let scr = document.createElement('script');
     let inner = document.createElement('script');
-    scr.src = "https://ads-partners.coupang.com/g.js";
     inner.innerHTML = `new PartnersCoupang.G({"id":629094,"template":"carousel","trackingCode":"AF7144675","width":"100%","height":"100%"})`;
-    document.querySelector('.coupang').appendChild(scr);
     document.querySelector('.coupang').appendChild(inner);
 
   }, [])

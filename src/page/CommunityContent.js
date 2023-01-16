2658230364447679c9bbc5eaa8f6b9cb1ff4edc0
kimcheolgi <kimchelgi03@gmail.com>
2023-01-16 16:37:52 +0900

    게시물 광고 추가

--- src/page/CommunityContent.js
+++ src/page/CommunityContent.js
@@ -509,6 +509,22 @@ function CommunityContent() {
           삭제
         </Button> : null 
       }
+    <div className='mt-5'>
+      <AdSense.Google
+        style={{ display: 'block' }}
+        client='ca-pub-3213525149688431'
+        slot='5111538528'
+        format='auto'
+        responsive="true"
+      />
+      {/* <AdSense.Google
+        style={{ display: 'block' }}
+        client='ca-pub-3213525149688431'
+        slot='1373843183'
+        format='autorelaxed'
+        // responsive="true"
+      /> */}
+    </div>
     </div>
   )
   
